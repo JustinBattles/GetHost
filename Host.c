d0e8d1e066dc9291002a36269b157485593527f7
Justin Battles <jbattles393@gmail.com>
2019-11-29 21:59:11 -0500

    Update Host.c

--- Host.c
+++ Host.c
@@ -11,40 +11,15 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
-/* Includes go here */
 
-/*
- * List who you worked with here:
- *
- */
-
-/* host.c: (3pts)
- *   A program to print the IP address of a host specified on the command
- *   line.  If no argument is provided, print a usage message and exit.
- * Example input/output:
- * > ./host www.indstate.edu
- * Host: www-prod-cms-lb.indstate.edu
- * Aliases:
- *   www.indstate.edu
- * Host type: IPv4
- * Addresses:
- *   139.102.15.65
- */
 
 int main(int argc, char *argv[])
-{
-  /* man 3 gethostbyname for information on the hostent structure: */
+{  
 
   if (argv[1] == NULL) {
     printf("Usage: ./a.out <host to check>\n"); 
     exit(1);
   }
-  /**
-   * Use the gethostbyname(3) function to look-up the host provided on the
-   * first command line argument (argv[1]), assign the result to host.  If
-   * host is NULL, print "Host not found: " followed by name of the host and
-   * exit with a value of 1:
-   */
 
 	struct hostent *he = gethostbyname(argv[1]);
 
@@ -54,14 +29,9 @@ int main(int argc, char *argv[])
 	}
 
 
-  /* Print the name of the host: */
   printf("Host: %s\n", he->h_name);
   
 
-  /**
-   * If h_aliases[0] is not null, then print a list of aliases, otherwise don't:
-   * (there may be more than one, ex: cs.indstate.edu while logged into CS)
-   */
  if (he->h_aliases[0] != NULL) { 
    printf("Aliases: \n");
    
@@ -70,7 +40,6 @@ int main(int argc, char *argv[])
 
  }
   
-  /* Print the host type (IPv4 or IPv6). */
 
 
  if (he->h_addrtype == AF_INET)
@@ -78,12 +47,6 @@ int main(int argc, char *argv[])
  else if (he->h_addrtype == AF_INET6) 
    printf("Host type: IPv6\n");
  
-
-  /**
-   * Print the addresses, there may be more than one: (ex: amazon.com)
-   * Use the inet_ntop(3) function to write the IP-address into a buffer, then
-   * print the string found in the buffer.
-   */
 
   char buf[INET_ADDRSTRLEN];
   
